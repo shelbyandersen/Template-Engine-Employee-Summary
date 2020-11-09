4de793eb47c27cfeb38bfd0e2a4d8357a0f04471
Shelby <shelbyrobison14@gmail.com>
2020-11-08 20:31:55 -0500

    added employee class

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,4 +1,25 @@
-// Require inquirer and fs
-const inquirer = require("inquirer");
-const fs = require("fs");
+// TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email){
+        this.name = test;
+        this.id = id;
+        this.email = email;
+    }
+}
 
+module.exports = Employee;
+
+// class Shape {
+//     constructor(area, perimeter) {
+//       this.area = area;
+//       this.perimeter = perimeter;
+//     }
+  
+//     printInfo() {
+//       for (const key in this) {
+//         console.log(`${key}: ${this[key]}`);
+//       }
+//     }
+//   }
+  
+//   module.exports = Shape;
