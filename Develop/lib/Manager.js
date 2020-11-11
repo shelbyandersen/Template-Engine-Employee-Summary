61a7a5a0e5175c8a198a1a938c030be5d3861845
Shelby <shelbyrobison14@gmail.com>
2020-11-10 22:35:59 -0500

    getOffice number has passed test in manager test

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,19 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require ("./Employee.js");
+
+class Manager extends Employee {
+  constructor(name, id, email, officenumber) {
+    super(name, id, email);
+    this.officenumber = officenumber;
+  }
+
+  getOfficeNumber(){
+      return this.officenumber;
+  }
+
+}
+
+module.exports = Manager;
+
+
