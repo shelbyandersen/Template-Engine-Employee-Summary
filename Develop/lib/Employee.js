c01c810d879900e95ff945954113261b6186f918
Shelby <shelbyrobison14@gmail.com>
2020-11-10 21:45:14 -0500

    test for name has passed

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,7 +1,7 @@
 // TODO: Write code to define and export the Employee class
 class Employee {
     constructor(name, id, email){
-        this.name = test;
+        this.name = name;
         this.id = id;
         this.email = email;
     }
