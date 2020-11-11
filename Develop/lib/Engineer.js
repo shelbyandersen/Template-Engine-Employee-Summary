b5a6d614bcbf501cb5eb58c10fa47d8ded61f4f9
Shelby <shelbyrobison14@gmail.com>
2020-11-10 22:19:49 -0500

    getRole test passed in engineer test - all Engineer tests passed

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -6,6 +6,11 @@ class Engineer extends Employee {
     super(name, id, email);
     this.github = github;
   }
+
+  getRole(){
+      return "Engineer";
+  }
+
   getGithub() {
     return this.github;
   }
