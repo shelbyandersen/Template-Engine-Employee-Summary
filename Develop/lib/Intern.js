bfa419741013a0b2619532de20413cc927d99b71
Shelby <shelbyrobison14@gmail.com>
2020-11-10 22:24:36 -0500

    first test passed for intern, added school

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,13 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require ("./Employee.js");
+
+class Intern extends Employee {
+  constructor(name, id, email, school) {
+    super(name, id, email);
+    this.school = school;
+  }
+
+
+}
+
+module.exports = Intern;
