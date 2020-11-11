0767a84d288b53196cde120f07bae20b84376ee1
Shelby <shelbyrobison14@gmail.com>
2020-11-11 15:56:08 -0500

    manager, employee, and intern files are required. Questions are prompted for the manager to build team

--- Develop/app.js
+++ Develop/app.js
@@ -13,10 +13,75 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+const teamMembers = [];
+const emptyId=[];
+
+const questionsEmployee = [
+    {
+        type: "input",
+        name: "nameManager",
+        message: "What is the manager's name?"
+    },
+    {
+        type: "input",
+        name: "managerId",
+        message: "What is the manager's ID?"
+    },
+    {
+        type: "input",
+        name: "emailManager",
+        message: "What is the manager's email?"
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the manager's office number?"
+    }
+];
+
+function manager() {
+    console.log("Let's build your team");
+    inquirer.prompt(questionsEmployee).then(function(data){
+        const manager = new Manager(data.nameManager, data.managerId, data.emailManager, data.officeNumber);
+        teamMembers.push(manager);
+        emptyId.push(data.managerId);
+        team();
+    });
+};
+
+function team() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "memberChoice",
+            message: "Which type of member would you like to add?",
+            choices: [
+                "Engineer",
+                "Intern",
+                "I don't want to add any more team members"
+            ]
+        }
+    ]).then(function(data){
+        if (data.memberChoice === "Engineer"){
+            engineer();
+        } else if (data.memberChoice === "Intern"){
+            intern();
+        } else (outputTeam());
+    });
+};
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
+
+function outputTeam() {
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR)
+    }
+    fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+}
+
+manager();
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
